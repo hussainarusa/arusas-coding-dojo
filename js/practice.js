be8a87d1105fe8fa861e5b1fbe0824b8339378b8
Arusa Hussain <hussainarusa18@gmail.com>
2023-11-01 22:00:00 +0530

    js funct

--- js/practice.js
+++ js/practice.js
@@ -3,3 +3,19 @@ function hello (){
     let i= "hello"
 }
 console.log(hello)
+
+function changeColor() {
+
+    let selected = document.getElementById('first');
+
+    selected.style.color = 'red';
+
+}
+
+
+document.addEventListener('click', function(event) {
+    if (!event.target.matches('p')) {return;}
+    event.target.style.color = 'red';
+});
+
+console.log(changeColor)
