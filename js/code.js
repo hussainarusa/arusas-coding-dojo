a0f3b80d97798030b5f5dd6b659a7f00c75f5d9b
Arusa Hussain <hussainarusa18@gmail.com>
2023-12-12 22:00:00 +0530

    daily commit

--- js/code.js
+++ js/code.js
@@ -1,9 +1,24 @@
 // Find a pair with the given sum in an array
 // Given an unsorted integer array, find a pair with the given sum in it.
 
-function findSumPair(array,int){
+function findSumPair(array, targetSum){
+    let int1ind, int2ind;
+    // to store the indexes
+    // iterate through array
+     array = [1, 2, 3, 4, 5, 6];
+     targetSum = 7
 
+    for (let i = 0; i > array.length; i++){
+        console.log(array[i]);
+    }
+
+
+
+
+
+    // return (int1, int)
 }
+
 
 // Pseudo:  Take in an array and target sum, sort through the integers of array,
 // add integers in array and compare to the sum int by comparing it to the num,
