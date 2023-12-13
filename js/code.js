d5e5ad7505499fb46e4165cbbcfb653ff9352e36
Arusa Hussain <hussainarusa18@gmail.com>
2023-12-13 22:00:00 +0530

    daily commit

--- js/code.js
+++ js/code.js
@@ -2,24 +2,25 @@
 // Given an unsorted integer array, find a pair with the given sum in it.
 
 function findSumPair(array, targetSum){
-    let int1ind, int2ind;
-    // to store the indexes
-    // iterate through array
-     array = [1, 2, 3, 4, 5, 6];
-     targetSum = 7
+    const numMap = {};
+//     this will store the numbers gone thru
+ for (let i = 0; i < array.length; i ++){
+     const currentVal = array[i]
+     const compliment = currentVal-targetSum
+     // Found a pair
+     if (numMap[compliment] !== undefined){
 
-    for (let i = 0; i > array.length; i++){
-        console.log(array[i]);
-    }
+         return [compliment, currentVal];
+     }
+     // Add the current number to the hash table
+     numMap[currentVal] = i
+ }
+
+  // if no pair is found
+  return null
 
 
-
-
-
-    // return (int1, int)
 }
-
-
 // Pseudo:  Take in an array and target sum, sort through the integers of array,
 // add integers in array and compare to the sum int by comparing it to the num,
 // the function should return all integers in the array that give the target sum
