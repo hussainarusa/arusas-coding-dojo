a165c2cc8cd977bf84cd2950cd77dac83a2de13a
Arusa Hussain <hussainarusa18@gmail.com>
2024-01-12 21:59:38 -0600

    prep set up

--- js/tetris.js
+++ js/tetris.js
@@ -3,5 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const width= 10
     const height= 20
 
-    const
+    const Tetrominoes = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2]
+        [1, width+1, width*2+1 , width*2],
+        [width, width*2, width*2+2]
+    ]
+
+
 })
