66316e270fb64dde20c5ab5541067510b480df43
Arusa Hussain <hussainarusa18@gmail.com>
2024-01-16 13:44:29 -0600

    algorithms

--- js/code.js
+++ js/code.js
@@ -45,3 +45,7 @@ if (result) {
 // Output: Subarray with zero-sum exists
 // The subarrays with a sum of 0 are:
 // { 3, 4, -7 }{ 4, -7, 3 }{ -7, 3, 1, 3 }{ 3, 1, -4 }{ 3, 1, 3, 1, -4, -2, -2 }{ 3, 4, -7, 3, 1, 3, 1, -4, -2, -2 }
+
+//pseudocode:
+// A function that takes in an
+
---
+++ linear_search.js
@@ -0,0 +1,11 @@
+// Linear Search Implementations
+
+
+function linearSearch (array, key){
+    for (let i = 0; i < array.length; i++){
+        if (array[i]===key){
+            return i;
+        }
+    }
+    return -1;
+}
